6ffa863866302ccde3148a53ecd2ac69fe849439
Rebecca Amarh-Adjei <37591664+ramarhadjei@users.noreply.github.com>
2020-07-22 14:59:59 -0400

    Add score

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 let score = 0;
+
+//Var holes = document.getElementsByClassName('hole');
+//console.log
+var scoreDisplay = document.getElementById('score');
+
+console.log(scoreDisplay);
+console.log('line', score);
